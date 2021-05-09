1e8b76dab528e57b8bd7396a86c9a890c747378f
PushingTrooper <junis.milori@gmail.com>
2021-05-09 23:53:18 +0200

    Actually made something work, we've got our first API BOYYYYYYYYYYYYS

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Perdorues;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
-    //
+    public function getAllUsers()
+    {
+        return ["users" => Perdorues::all()];
+    }
 }
