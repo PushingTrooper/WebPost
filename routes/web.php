6c5ea3436e32575bd61d0fcd44194fe2738fc9af
PushingTrooper <junis.milori@gmail.com>
2021-05-13 14:17:11 +0200

    Created views and added routes to the views

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,82 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/track', function () {
+    return view('tracking_page');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+/** ONLY ADMIN **/
+
+Route::get('/admin', function () {
+    return view('admin.admin_dashboard');
+});
+
+Route::get('/admin/changePassword', function () {
+    return view('admin.change_password');
+});
+
+Route::get('/admin/locatePostmen', function () {
+    return view('admin.locate_all_postmen');
+});
+
+Route::get('/admin/createUser', function () {
+    return view('admin.create_new_user');
+});
+
+Route::get('/admin/editUser', function () {
+    return view('admin.change_pay_and_role');
+});
+
+/** EMPLOYEES **/
+
+Route::get('/packages', function () {
+    return view('packages_list');
+});
+
+Route::get('/packageDetails', function () {
+    return view('package_details');
+});
+
+Route::get('/editPackage', function () {
+    return view('edit_package');
+});
+
+Route::get('/post/warehouses', function () {
+    return view('postmen.warehouses_location');
+});
+
+Route::get('/warehouse/logistics', function () {
+    return view('warehouse.logistics');
+});
+
+Route::get('/warehouse/reportLost', function () {
+    return view('warehouse.report_lost_package');
+});
+
+Route::get('/requestPasswordChange', function () {
+    return view('request_password_change');
+});
+
+/** ONLY COSTUMERS **/
+
+Route::get('/client/createPackage', function () {
+    return view('client.create_new_package');
+});
+
+Route::get('/client/editPackage', function () {
+    return view('client.edit_package_details');
+});
+
+Route::get('/client/deletePackage', function () {
+    return view('client.delete_package');
+});
+
+Route::get('/client/makePayment', function () {
+    return view('client.make_payment');
+});
