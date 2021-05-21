da1d457cb9faedb2d14fba5f0cb7555651cee9b4
tedi00 <tedi.skenduli@yahoo.com>
2021-05-22 01:35:57 +0200

    fonts, scripts, styles, layout view, login edits
    added jquery, some scss/css, added fonts, added layouts/master, edited login, added two images to public/images in storage
    note: login not operational

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>WebPost</title>
+    {{--  Fonts  --}}
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    {{--  Styles  --}}
+    <link rel="stylesheet" href="{{ asset('css/normalize.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}"/>
+    {{--  Scripts (defer)  --}}
+    <script src="{{ asset('js/jquery-3.6.0.min.js') }}"></script> {{--  do not defer jQuery  --}}
+    <script defer src="{{ asset('bootstrap/js/bootstrap.min.js') }}"></script>
+</head>
+<body>
+{{--  can @include() other partials  --}}
+<div id="content">
+    @yield('content')
+</div>
+</body>
+</html>
